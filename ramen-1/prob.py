b358ede1644af272bd033d594bb35444f22d59d2
JACazares <josecazares5305@gmail.com>
2023-01-10 11:35:59 -0500

    change prob.py

--- ramen-1/prob.py
+++ ramen-1/prob.py
@@ -1,2 +1,11 @@
+import eval7
+
 def return_probabilities(board, street):
-    
+    pass
+
+def get_playable_hands():
+    deck = eval7.Deck()
+    for i in deck:
+        print(type(i))
+
+get_playable_hands()
