675bb2f9480ca08500b9f524a8f908b2cdc5d533
JACazares <josecazares5305@gmail.com>
2023-01-10 11:41:13 -0500

    test prob.py

--- ramen-1/prob.py
+++ ramen-1/prob.py
@@ -7,6 +7,6 @@ def get_playable_hole_cards():
     deck = eval7.Deck()
     for c1 in deck:
         for c2 in deck:
-            
+            pass
 
-get_playable_hands()
+get_playable_hole_cards()
