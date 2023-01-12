614c97866d3bfb69d5fc3c0efde32a4ce06ed117
Tomas <tomascr@mit.edu>
2023-01-12 16:37:28 -0500

    ljhdlfkb

--- ramen-3/prob.py
+++ ramen-3/prob.py
@@ -4,6 +4,15 @@ import pandas as pd
 def get_strength(prob_table, hole):
     return prob_table[(hole[0], hole[1])]
     
+def get_opp_range(hole, board, opp_contribution, ):
+    '''
+        if opponent calls our bet, we assume they think their p-winning is greater than their pot odds.
+        In that case, we update their range accordingly. 
+
+        if opponent raises, we assume they think our pot odds now is greater than their p winning
+    '''
+    pass
+
 def get_all_hands(prob_table):
     deck = eval7.Deck()
     all_hands = []
