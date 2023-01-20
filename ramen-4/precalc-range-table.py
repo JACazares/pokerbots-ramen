b1df1d1fcc3c3bcdd50969e30f266f18f2973033
JACazares <josecazares5305@gmail.com>
2023-01-20 14:38:22 -0500

    create hand_range preflop table

--- ramen-4/precalc-range-table.py
+++ ramen-4/precalc-range-table.py
@@ -2,6 +2,7 @@ import eval7
 
 def range_table():
     deck = eval7.Deck()
+    hand_range = {}
     for i in range(52):
         for j in range(i, 52):
             if i == j:
@@ -9,6 +10,7 @@ def range_table():
             r = (str(deck.cards[i])[0], str(deck.cards[j])[0])
             s = (str(deck.cards[i])[1], str(deck.cards[j])[1])
 
+            ans = 8
             # Pairs
             if r[0] == r[1]:
                 if r[0] == '2':
@@ -19,7 +21,6 @@ def range_table():
                     ans = 2
                 else:
                     ans = 1
-                print(ans)
             # Suited
             elif s[0] == s[1]:
                 if r[0] == '2':
@@ -120,18 +121,73 @@ def range_table():
                     else:
                         ans = 3
                 elif r[0] == '4':
+                    if r[1] in ['5', '6', '7']:
+                        ans = 8
+                    elif r[1] in ['8', '9', 'T']:
+                        ans = 7
+                    elif r[1] in ['J', 'Q', 'K']:
+                        ans = 5
+                    else:
+                        ans = 3
                 elif r[0] == '5':
+                    if r[1] in ['6']:
+                        ans = 8
+                    elif r[1] in ['7', '8', '9', 'T']:
+                        ans = 7
+                    elif r[1] in ['J', 'Q']:
+                        ans = 5
+                    else:
+                        ans = 3
                 elif r[0] == '6':
+                    if r[1] in ['7', '8', '9', 'T']:
+                        ans = 6
+                    elif r[1] in ['J', 'Q']:
+                        ans = 5
+                    else:
+                        ans = 3
                 elif r[0] == '7':
+                    if r[1] in ['8', '9', 'T']:
+                        ans = 6
+                    elif r[1] in ['J', 'Q']:
+                        ans = 5
+                    else:
+                        ans = 3
                 elif r[0] == '8':
+                    if r[1] in ['9', 'T']:
+                        ans = 6
+                    elif r[1] in ['J', 'Q']:
+                        ans = 4
+                    else:
+                        ans = 3
                 elif r[0] == '9':
+                    if r[1] in ['T', 'J', 'Q']:
+                        ans = 4
+                    elif r[1] in ['K']:
+                        ans = 3
+                    else:
+                        ans = 2
                 elif r[0] == 'T':
+                    if r[1] in ['J', 'Q']:
+                        ans = 4
+                    elif r[1] in ['K']:
+                        ans = 3
+                    else:
+                        ans = 2
                 elif r[0] == 'J':
-                elif r[0] == 'Q':
-                elif r[0] == 'K':
-                elif r[0] == 'A':
+                    if r[1] in ['Q']:
+                        ans = 4
+                    else:
+                        ans = 2
+                else:
+                    ans = 2
+
+            hand_range[(str(deck.cards[i]), str(deck.cards[j]))] = ans
+            hand_range[(str(deck.cards[j]), str(deck.cards[i]))] = ans
+    
+    return hand_range
 
 
 if __name__ == "__main__":
-    range_table()
+    hand_range = range_table()
+    print(hand_range)
     pass
